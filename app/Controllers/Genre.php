1fa88ee9c7eb2b5145a66e666d5e00a1e1f10219
thehrgn <fernando031185@gmail.com>
2021-01-12 10:06:57 -0600

    'Actualizacion'

--- app/Controllers/Genre.php
+++ app/Controllers/Genre.php
@@ -42,10 +42,32 @@ echo $parser->setData($data)
 
 	public function edit($id){
 
+		$gm = new GenresModel();
+		$gr = $gm->find($id);
+		$name = $gr->name;
+
 		$parser = \Config\Services::parser();
-		echo $parser->setData(['titulo'=>'Edit Genre','id'=>$id,'name'=>''])
+		echo $parser->setData(['titulo'=>'Edit Genre','id'=>$id,'name'=>$name,'base_url'=>base_url()])
 		             ->render('edit');
 			}
+
+			public function update(){
+				if ($this->request->getMethod() === 'post' &&
+					$this->validate(['name' => 'required|min_length[3]|max_length[255]','id'=>'required|integer'])
+				)
+			 {//Obtenemos valores de los inputs
+				 $id = $this->request->getPost('id');
+				 $name = $this->request->getPost('name');
+				 //Instancia del modelo
+				 $gm = new GenresModel();				 
+
+				 $gm->update($id,['name'=>$name,]);
+				 $this->response->redirect('index');
+
+			 }else{
+				 echo 'Verifique datos';
+			 }
+		}
 
 
 }
--- app/Views/edit.php
+++ app/Views/edit.php
@@ -7,7 +7,7 @@
   <body>
     <h1>{titulo}</h1>
     <h2></h2>
-    <form class="" action="save" method="post">
+    <form class="" action="{base_url}/disco/public/genre/update" method="post">
       <label for="name">Name:</label>
       <input type="text" name="name" id="name" value="{name}" minlength="3" maxlength="255" required>
       <input type="hidden" name="id" id="id" value="{id}">
